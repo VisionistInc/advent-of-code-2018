10816285812bc5b6ee3baa075e06983498e8cf3d
Dillon Mulroy <dillon.mulroy@gmail.com>
2018-12-01 22:47:30 -0500

    remove loop from part 1

--- dmmulroy/01/part1.js
+++ dmmulroy/01/part1.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 
-const output = input
-  .split('\n')
-  .map(Number)
-  .reduce((prev, curr) => prev + curr, 0);
+const output = input.split('\n').reduce((prev, curr) => prev + Number(curr), 0);
 
 console.log(`output: ${output}`);
